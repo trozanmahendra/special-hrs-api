1f169f4d9743b02620f0a2b25ea97b18ec9e8926
Mahendra Goud <mahendra.goud@proeffective.in>
2024-10-15 18:07:54 +0530

    changes

--- src/main/java/com/supply_house/special_hrs/entity/RegularHour.java
+++ src/main/java/com/supply_house/special_hrs/entity/RegularHour.java
@@ -3,8 +3,10 @@ package com.supply_house.special_hrs.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "regular_hours",schema = "supply_house")
 public class RegularHour {
     @Id
     @Column(name = "id")
